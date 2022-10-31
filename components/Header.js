ab5442d60e09fd48c8775be834a03e0c955e7bc7
Iamweird2 <87063798+Iamweird2@users.noreply.github.com>
2022-10-31 18:13:16 +0100

    changed path to sidebarslice

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../states/sidebarSlice";
+import { toggle } from "../states/SidebarSlice";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
